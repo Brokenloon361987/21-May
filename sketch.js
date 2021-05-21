12b3d995e815f6b079c63c6a60fb0da7cea15f64
Brokenloon361987 <67371601+Brokenloon361987@users.noreply.github.com>
2021-05-21 19:58:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,108 @@
+ Engine = Matter.Engine,
+ World = Matter.World,
+ Events = Matter.Events,
+ Bodies = Matter.Bodies;
+ Constraint = Matter.Constraint;
+ 
+var particles;
+var plinkos = [];
+
+var Play, End
+var gameState = Play
+var divisions = [];
+var turn;
+
+var divisionHeight=300;
+var score =0;
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+    divisions.push (new Divisionz(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+}
+ 
+function draw() {
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+  //  if(frameCount%60===0){
+  //    particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+  //    score++;
+  //  }
+ 
+  // for (var j = 0; j < particles.length; j++) {
+   
+  //    particles[j].display();
+  //  }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+
+   if(gameState === Play){
+      turn = 5
+   }
+  
+   if(gameState === Play /*&& mousePressed()*/){
+
+    particle = new Particle(mouseX, 10, 10, 10)
+    turn = turn - 1;
+
+   }
+
+   if(turn === 0){
+
+   gameState = End;
+
+   }
+
+}
+
+// function mousePressed() {
+
+// if(gameState!==End) {
+//   count++;
+//   particles = new Particle(10, 10, 10, 10);
+// }
+
+// }
